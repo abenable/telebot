b28013b40c9877991f9905d716d7454e1b75411d
Able Abenaitwe <ableabenaitwe@gmail.com>
2023-06-16 19:52:43 +0300

    added chat gpt

---
+++ src/controllers/telegramBot.js
@@ -0,0 +1,58 @@
+import TelegramBot from 'node-telegram-bot-api';
+import { Configuration, OpenAIApi } from 'openai';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { sendwhatsappmsg } from './whatsappControlller.js';
+
+const token = process.env.TOKEN;
+const apiKey = process.env.OPENAI_API_KEY;
+
+export const bot = new TelegramBot(token, { polling: true });
+
+const configuration = new Configuration({ apiKey });
+const openai = new OpenAIApi(configuration);
+
+const chat_completion = await openai.createChatCompletion({
+  model: 'gpt-3.5-turbo',
+  messages: [{ role: 'user', content: 'hello world' }],
+});
+
+const url = process.env.URL;
+
+bot.setWebHook(`${url}/bot${token}`);
+
+bot.onText(/\/start/, async (msg) => {
+  bot.sendMessage(msg.chat.id, `Hello ${msg.from.first_name}`, {
+    reply_markup: {
+      keyboard: [['/chat', '/start']],
+    },
+  });
+});
+
+bot.onText(/\/sendmessage/, async (msg) => {
+  await bot.sendMessage(msg.chat.id, 'This command is not working');
+});
+
+bot.onText(/\/chat/, async (msg) => {
+  await bot.sendMessage(msg.chat.id, 'This command is working');
+
+  bot.on('message', async (msg) => {
+    if (!String(msg.text).includes('/')) {
+      const chat_completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: msg.text }],
+      });
+      await bot.sendMessage(
+        msg.chat.id,
+        chat_completion.data.choices[0].message.content + '\nSent to whatsapp.'
+      );
+      await sendwhatsappmsg(msg.text);
+    }
+  });
+});
+
+bot.on('photo', async (msg) => {
+  await bot.sendPhoto(msg.chat.id, msg.photo.at(0).file_id);
+});
--- src/index.js
+++ src/index.js
@@ -1,53 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import nodeTelegramBotApi from 'node-telegram-bot-api';
 import bodyParser from 'body-parser';
-import { sendwhatsappmsg } from './controllers/whatsappControlller.js';
-import { teleRouter } from './routes/tele.js';
+import { bot } from './controllers/telegramBot.js';
 
 dotenv.config();
 
-const token = process.env.TOKEN;
 const port = process.env.PORT;
-const url = process.env.URL;
-console.log('Sent to whatsapp.\nThis command is still under developent');
 
 const app = express();
-const bot = new nodeTelegramBotApi(token, { polling: true });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-bot.setWebHook(`${url}/bot${token}`);
-
-bot.onText(/\/start/, async (msg) => {
-  bot.sendMessage(msg.chat.id, `Hello ${msg.from.first_name}`, {
-    reply_markup: {
-      keyboard: [['sample text', 'sample text2']],
-    },
-  });
-});
-
-bot.onText(/\/sendmessage/, async (msg) => {
-  await bot.sendMessage(msg.chat.id, 'This command is developed');
-});
-
-bot.on('message', async (msg) => {
-  if (!String(msg.text).includes('/')) {
-    await sendwhatsappmsg(msg.text);
-    await bot.sendMessage(msg.chat.id, msg.text + '\nSent to whatsapp.');
-  }
-});
-
-bot.on('photo', async (msg) => {
-  await bot.sendPhoto(msg.chat.id, msg.photo.at(0).file_id);
-});
-app.use('/', teleRouter);
-
-app.post(`/bot${token}`, (req, res) => {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
+bot;
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
